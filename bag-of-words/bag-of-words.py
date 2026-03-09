af3e31aef9ba6f5620d748b82eea79037f23f0df
DRISHYA GARG <drishyagarg22@gmail.com>
2026-03-09 15:46:12 +0530

    Update solution for bag-of-words

--- bag-of-words/bag-of-words.py
+++ bag-of-words/bag-of-words.py
@@ -1,11 +1,10 @@
 import numpy as np
+from collections import Counter
 
 def bag_of_words_vector(tokens, vocab):
     """
     Returns: np.ndarray of shape (len(vocab),), dtype=int
     """
     # Your code here
-    result=[]
-    for i in vocab:
-        result.append(tokens.count(i))
-    return np.array(result,dtype="int")
+    freq=Counter(tokens)
+    return np.array([freq.get(word,0) for word in vocab],dtype=int)
